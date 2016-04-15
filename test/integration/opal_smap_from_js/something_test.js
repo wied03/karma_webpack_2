141ec0442bc8f400f2b05315f95b4beb01f91be7
Brady Wied <brady@bswtechconsulting.com>
2016-04-14 19:13:35 -0600

    Add more of the test implementation, close to making the fix

--- test/integration/opal_smap_from_js/something_test.js
+++ test/integration/opal_smap_from_js/something_test.js
@@ -1,12 +1,33 @@
 'use strict'
 
 require('./dependency.rb')
-
-// TODO: Use JS stacktrace here and assert it directly
+// PhantomJS
+require('es6-promise').polyfill()
+var StackTrace = require('stacktrace-js')
 
 describe('A suite', function() {
-  it('contains spec with an expectation', function() {
-    var result = Opal.Foobar.$howdy()
-    expect(99).toBe(42)
+  it('contains spec with an expectation', function(done) {
+    try {
+      Opal.Foobar.$howdy()
+      // expected exception, didn't get one
+      expect(true).toBe(false)
+    }
+    catch (e) {
+      StackTrace.fromError(e).then(function(frames) {
+        // TODO: We need to locate the correct frame and we need to add middleware to serve up
+        // the map file on request
+        frames.forEach(function (frame) {
+          console.log('saw file '+frame.fileName)
+        })
+        var frame = frames[1]
+        expect(frame.fileName).toBe('dependency.rb')
+        expect(frame.functionName).toBe('howdy')
+        expect(frame.lineNumber).toBe(4)
+        return done()
+      }).catch(function(problem) {
+        console.log('could not get stacktrace')
+        done(problem)
+      })
+    }
   })
 })
