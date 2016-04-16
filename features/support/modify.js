c3f589cb99b8a14bc32f7b2b8bb2d1e530a56ac4
Brady Wied <brady@bswtechconsulting.com>
2016-04-16 14:41:22 -0600

    Entry point test passes

--- features/support/modify.js
+++ features/support/modify.js
@@ -14,4 +14,21 @@ module.exports = function () {
     '})'
     return writeFile(newSpecFile, specText)
   })
+
+  this.When(/^I add a new spec to the entry point$/, function () {
+    const newSpecFile = path.join(this.testDir, 'new_other_suffix.js')
+    const entryPoint = path.join(this.karmaTmpDir, 'entry_point.js')
+    const specText = "describe('A suite', function() {\n" +
+    "it('contains spec 2 with an expectation', function() {\n" +
+    "expect('foobar').toBe('foobar')\n" +
+    '})\n' +
+    '})'
+    return writeFile(newSpecFile, specText).then(function() {
+      const newEntryPointFile = "var testsContext = require.context('./test', true, /_test\.js$/)\n" +
+      'testsContext.keys().forEach(testsContext)\n' +
+      "var moreTests = require.context('./test', true, /_other_suffix\.js$/)\n" +
+      'moreTests.keys().forEach(moreTests)'
+      return writeFile(entryPoint, newEntryPointFile)
+    })
+  })
 }
